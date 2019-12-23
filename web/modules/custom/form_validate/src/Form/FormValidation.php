2abf488534ef2bf5dc5ab6e38e34a097a87abc09
Rozhii Olexandr <rozhiisasha@gmail.com>
2019-12-23 15:44:41 +0200

    Add function for validate rows, table. Correct inline comment. Omptimized code.

--- web/modules/custom/form_validate/src/Form/FormValidation.php
+++ web/modules/custom/form_validate/src/Form/FormValidation.php
@@ -13,6 +13,8 @@ use Drupal\Core\Form\FormStateInterface;
 class FormValidation extends FormBase {
 
   /**
+   * Variable $year.
+   *
    * @var int
    *  Current year.
    */
@@ -29,7 +31,7 @@ class FormValidation extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $this->year = date('Y');
+    $this->year = $this->getYear();
     $num_of_table = $form_state->get('num_of_table');
     if (empty($num_of_table)) {
       $num_of_table = 0;
@@ -64,18 +66,19 @@ class FormValidation extends FormBase {
       ];
       $year = $form_state->get('year');
       if (empty($year[$y])) {
-        $year[$y] = date('Y');
+        $year[$y] = $this->getYear();
         $form_state->set('year', $year);
       }
-      $num_of_rows = date('Y') + 1 - $year[$y];
+
+      $num_of_rows = $this->getYear() + 1 - $year[$y];
       for ($i = 1; $i <= $num_of_rows; $i++) {
         $year = $this->year - $i + 1;
         foreach ($mounth as $key => $value) {
-          if ($_POST[$y . '_' . $i . '_' . $key] != NULL) {
+          if (!empty($_POST[$y . '_' . $i . '_' . $key])) {
             $value_k = $_POST[$y . '_' . $i . '_' . $key];
           }
           else {
-            if ($_POST[$key . $y . $i] != NULL) {
+            if (!empty($_POST[$key . $y . $i])) {
               $value_k = $_POST[$key . $y . $i];
             }
             else {
@@ -106,27 +109,27 @@ class FormValidation extends FormBase {
           }
           elseif ($key == 'ytd') {
             $form['mounth'][$y][$i][$key] = [
-              '#type'     => 'textfield',
-              '#size'     => '4',
-              '#value'    => $value_k,
-              '#step'     => 0.001,
-              '#min'      => 0,
+              '#type'       => 'textfield',
+              '#size'       => '4',
+              '#value'      => $value_k,
+              '#step'       => 0.001,
+              '#min'        => 0,
               '#attributes' => [
                 'readonly' => TRUE,
               ],
-              '#name'     => $y . '.' . $i . '.' . $key,
-              '#id'       => $y . $i . $key,
+              '#name'       => $y . '.' . $i . '.' . $key,
+              '#id'         => $y . $i . $key,
             ];
           }
           else {
             $form['mounth'][$y][$i][$key] = [
-              '#type'       => 'number',
-              '#min'        => 0,
-              '#step'     => 0.001,
-              '#width'      => 4,
-              '#value'      => $value_k,
-              '#id'         => $y . '.' . $i . '.' . $key,
-              '#name'       => $y . '.' . $i . '.' . $key,
+              '#type'  => 'number',
+              '#min'   => 0,
+              '#step'  => 0.001,
+              '#width' => 4,
+              '#value' => $value_k,
+              '#id'    => $y . '.' . $i . '.' . $key,
+              '#name'  => $y . '.' . $i . '.' . $key,
             ];
           }
         }
@@ -136,10 +139,9 @@ class FormValidation extends FormBase {
           '#name'   => 'new_row_to' . $y,
           '#submit' => ['::addRow'],
         ];
-
       }
-      $form['#tree'] = TRUE;
-      /*sort, because we must output table after*/
+
+      // sort, because we must output table after.
       krsort($form['mounth'][$y]);
     }
     $form['#attached']['library'][] = 'form_validate/form_validate';
@@ -150,128 +152,48 @@ class FormValidation extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    $form_state->set('valid', TRUE);
-    $ver = $form_state->getValues();
-    $tr_el = $form_state->getTriggeringElement()['#type']=='submit'?:NULL;
-    $ver = $ver['mounth'];
-//    $ver = array_filter($ver, $this->filterArray($ver));
-    foreach ($ver as $k => $v) {
-      if (!is_int($k)) {
-        unset($ver[$k]);
-      }
-      foreach ($ver[$k] as $k2 => $v2) {
-        if (!is_int($k2)) {
-          unset($ver[$k][$k2]);
-        }
-        foreach ($ver[$k][$k2] as $k3 => $v3) {
-          if (!is_int($k3)) {
-            unset($ver[$k][$k2][$k3]);
-          }
-          else {
-            if (!empty($v3)) {
-              $ver2[$k][$k2][$k3] = $v3;
 
+    $tr_el = $form_state->getTriggeringElement()['#name'] == 'new'
+      ?: NULL;
+    if ($tr_el) {
+      $t = 0;
+      $form_state->set('valid', TRUE);
+      foreach ($form_state->getUserInput() as $key => $value) {
+        // Explode $key from input, it was look: table_row_element.
+        $key_to_value = explode('_', $key);
+        // Validate number of table.
+        if ($t != $key_to_value[0] && $this->isNumeric($key_to_value[0])) {
+          unset($row);
+          unset($row2);
+          $t = $key_to_value[0];
+        }
+        // Create two array for validation.
+        if (!empty($key_to_value[2])) {
+          if ($this->isNumeric($key_to_value[2])) {
+            $row[] = $value;
+            // Ver - array for validate rows.
+            $ver[$key_to_value[0]] = $row;
+            if (!empty($value)) {
+              $arr2[] = $value;
             }
             else {
-              $ver2[$k][$k2]['empty'] = 0;
+              $arr2['empty'] = 0;
+            }
+            $row2[$key_to_value[1]] = $arr2;
+            // Ver2 - array fo validate table (contains only !empty variable).
+            $ver2[$key_to_value[0]] = $row2;
+            if ($key_to_value[2] == 12) {
+              unset($arr2);
             }
           }
         }
       }
-    }
-    if ($_POST['new'] == 'Submit') {
-      //      $t = 0;
-      //      foreach ($_POST as $key => $value) {
-      //        $key_to_value = explode('_', $key);
-      //        if ($t != $key_to_value[0]) {
-      //          unset($row);
-      //          $t = $key_to_value[0];
-      //        }
-      //        if (is_numeric($key_to_value[2])) {
-      //          $arr[] = $value;
-      //          $row[$key_to_value[1]] = $arr;
-      //          $table[$key_to_value[0]] = $row;
-      //          foreach ($arr as $k => $v) {
-      //            if ($key_to_value[2] == 12) {
-      //              unset($arr);
-      //            }
-      //          }
-      //        }
-      //      }
-      //      $t = 0;
-      //      $rows = 0;
-      //      for ($y = 0; $y < count($table); $y++) {
-      //        for ($r = 1; $r <= count($table[$y]); $r++) {
-      //          if ($t != $y) {
-      //            unset($arr1);
-      //            unset($row1);
-      //            $t = $y;
-      //          }
-      //          if ($rows != $r) {
-      //            unset($arr1);
-      //            $rows = $r;
-      //          }
-      //          foreach ($table[$y][$r] as $key => $val) {
-      //            if (is_numeric($val)) {
-      //              $arr1[$key] = $val;
-      //              $row1[$r] = $arr1;
-      //              $table1[$y] = $row1;
-      //            }
-      //            else {
-      //              $arr1['empty'] = 0;
-      //              $row1[$r] = $arr1;
-      //              $table1[$y] = $row1;
-      //            }
-      //          }
-      //        }
-      //      }
-      for ($y = 0; $y < count($ver); $y++) {
-        $arra = [];
-        for ($r = 1; $r <= count($ver[$y]); $r++) {
-          krsort($ver[$y][$r]);
-          $arra = array_merge($arra, $ver[$y][$r]);
-        }
-        $ver[$y] = $arra;
-        unset($arra);
-        for ($i = 0; $i < count($ver[$y]); $i++) {
-          if (is_numeric($ver[$y][$i])) {
-            $a = $i;
-            if (!is_numeric($ver[$y][$i + 1])) {
-              for ($i = $a + 1; $i < count($ver[$y]); $i++) {
-                if (is_numeric($ver[$y][$i])) {
-                  $form_state->set('valid', FALSE);
-                  break 2;
-                }
-              }
-            }
-          }
-        }
+      if (!empty($ver)) {
+        $this->validateRows($ver, $form_state);
       }
-
-      if ($form_state->get('valid')) {
-        for ($y = 0; $y < count($ver2); $y++) {
-          for ($i = 0; $i < count($ver2[$y]); $i++) {
-            if ($ver2[$y + 1] != NULL) {
-              $a = array_intersect_key($ver2[$y], $ver2[$y + 1]);
-              $b = array_intersect_key($ver2[$y + 1], $ver2[$y]);
-              foreach ($a as $ka => $va) {
-                foreach ($b as $kb => $vb) {
-                  if ($ka == $kb) {
-                    $a1 = array_diff_key($va, $vb);
-                    $b1 = array_diff_key($vb, $va);
-                    if ((!empty($a1)) || (!empty($b1))) {
-                      $form_state->set('valid', FALSE);
-                      break 4;
-                    }
-                  }
-                }
-              }
-            }
-            /*Output Invalid when table is empty*/
-            /*if (count($table1[$y][$i]) == 1) {
-            $form_state->set('valid', FALSE);
-            }*/
-          }
+      if (!empty($ver2)) {
+        if ($form_state->get('valid')) {
+          $this->validateTable($ver2, $form_state);
         }
       }
     }
@@ -294,40 +216,121 @@ class FormValidation extends FormBase {
 
   /**
    * Function addRow.
-   *  For add row to table.
    *
-   * @param  array  $form
-   * @param  \Drupal\Core\Form\FormStateInterface  $form_state
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
    */
   public function addRow(array &$form, FormStateInterface $form_state) {
     $year = $form_state->get('year');
-    /*find from what table we get request*/
+    // Find from what table we get request.
     foreach ($_POST as $key => $value) {
       if ($value == 'Add Year') {
         $key_name = substr($key, -1, 1);
-        /*$key_name now equal number table*/
+        // Key_name now equal number table.
         break;
       }
     }
     $year[$key_name]--;
     $form_state->set('year', $year);
-    /*rebuild table for output new row*/
+    // Rebuild table for output new row.
     $form_state->setRebuild();
   }
 
   /**
    * Function AddTable.
-   *  Add new table.
    *
-   * @param  array  $form
-   * @param  \Drupal\Core\Form\FormStateInterface  $form_state
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
    */
   public function addTable(array &$form, FormStateInterface $form_state) {
     $num_of_table = $form_state->get('num_of_table');
     $num_of_table++;
     $form_state->set('num_of_table', $num_of_table);
-    /*rebuild form for output new table*/
+    // Rebuild table for output new table.
     $form_state->setRebuild();
+  }
+
+  /**
+   * Function validateRows.
+   *
+   * @param  array  $ver
+   *   Array fo validation.
+   *
+   * @param  \Drupal\Core\Form\FormStateInterface  $form_state
+   */
+  public function validateRows(array $ver, FormStateInterface $form_state) {
+    for ($y = 0; $y < count($ver); $y++) {
+      for ($i = 0; $i < count($ver[$y]); $i++) {
+        // Search first number in table.
+        if ($this->isNumeric($ver[$y][$i])) {
+          // Remember key.
+          $a = $i;
+          // If next element not numeric, search from next`s element.
+          if (!$this->isNumeric($ver[$y][$i + 1])) {
+            for ($i = $a + 1; $i < count($ver[$y]); $i++) {
+              if ($this->isNumeric($ver[$y][$i])) {
+                $form_state->set('valid', FALSE);
+                return;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Function validateTable.
+   *
+   * @param  array  $ver2
+   *   Array fo validation.
+   *
+   * @param  \Drupal\Core\Form\FormStateInterface  $form_state
+   */
+  public function validateTable(array $ver2, FormStateInterface $form_state) {
+    foreach ($ver2 as $key => $value) {
+      if (!empty($ver2[$key + 1])) {
+        // Find common rows.
+        $b = array_intersect_key($ver2[$key + 1], $ver2[$key]);
+        $a = array_intersect_key($ver2[$key], $ver2[$key + 1]);
+        for ($i = 1; $i <= count($a); $i++) {
+          // In common rows, find is there different key.
+          $a1 = array_diff_key($a[$i], $b[$i]);
+          $b1 = array_diff_key($b[$i], $a[$i]);
+          // If common rows has different key - table non validate.
+          if ((!empty($a1)) || (!empty($b1))) {
+            $form_state->set('valid', FALSE);
+            return;
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Function getYear.
+   *
+   * @return mixed
+   *   Return current year.
+   */
+  public function getYear() {
+    $current_date = \Drupal::time()->getCurrentTime();
+    $year = \Drupal::service('date.formatter')
+      ->format($current_date, 'custom', 'Y');
+    return $year;
+  }
+
+  /**
+   * Function isNumeric.
+   *
+   * @param  mixed  $var
+   *   Variable for validate.
+   *
+   * @return bool
+   *   Return true if $var is numeric.
+   */
+  public function isNumeric($var) {
+    return is_numeric($var);
   }
 
 }
